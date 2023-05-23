61b8a3f615ff35fee1679aab8bffb87258479daa
karolrezende <karolrds01@gmail.com>
2023-05-23 18:39:05 -0400

    classification routes

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ import express, { Application, Router, json } from 'express'
 const app: Application = express()
 app.use(json())
 
-const useRouter: Router = Router()
+export const useRouter: Router = Router()
 
 app.use('/users', useRouter)
-
+app.use('/login', useRouter)
 export default app
 
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,2 +1,12 @@
 //neste ambiente eu comeco a ligar o tipo de requisição com a sua funcionalidade e middlewars
 
+import { useRouter } from "../app";
+
+
+useRouter.post('', ) //cadastrar um novo usuário
+useRouter.post('', ) //logar com usuário na aplicação retornando um token
+useRouter.get('', ) //listar todos os usuários na aplicação
+useRouter.get('/profile', ) //listar um usuário que está cadastrado na aplicação
+useRouter.patch('/:id', ) //atualizar os dados de um usuário
+useRouter.delete('/:id', ) //fazer um soft delete de um usuário
+useRouter.put('/:id/recover', ) //recuperar um usuário
