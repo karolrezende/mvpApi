94348ba03e14e549fc59c0ffb21b27379cba15b3
karolrezende <karolrds01@gmail.com>
2023-05-24 13:45:36 -0400

    add controllers user

---
+++ src/controllers/createUser.controllers.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+const createUser = async (req: Request, res: Response): Promise<Response> =>{
+    
+}
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,5 +1,4 @@
 //neste ambiente eu comeco a ligar o tipo de requisição com a sua funcionalidade e middlewars
-
 import { useRouter } from "../app";
 
 
